df75d3eb0ef666bf249ea37f6c99a71f89ce6fee
Francis Kayiwa <kayiwa@princeton.edu>
2019-01-10 15:18:51 -0500

    add elastic role with java dependencies

---
+++ roles/pulibrary.oracle.java8/molecule/default/tests/test_oracle_java8.py
@@ -0,0 +1,18 @@
+import os
+import pytest
+
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+@pytest.mark.parametrize("name", [
+    ("oracle-java8-installer")
+    ("ca-certificates")
+    ])
+def test_for_oracle_java8_installer(host, name):
+    package = host.package("name")
+
+    assert package.is_installed
