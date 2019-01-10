a05c6ed23a7298ba727491f514ffce4ae7ac077b
Francis Kayiwa <kayiwa@princeton.edu>
2019-01-10 10:36:53 -0500

    consistent name

--- roles/pulibrary.common/molecule/default/tests/test_common.py
+++ roles/pulibrary.common/molecule/default/tests/test_common.py
@@ -1,9 +1,25 @@
 import os
+import pytest
 
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+@pytest.mark.parametrize("name", [
+    "acl",
+    "build-essential",
+    "curl",
+    "git",
+    "unzip",
+    "zip",
+    "python-pip",
+    ])
+def test_commonly_needed_build_files(host, name):
+    pkg = host.package(name)
+
+    assert pkg.is_installed
 
 
 def test_hosts_file(host):
