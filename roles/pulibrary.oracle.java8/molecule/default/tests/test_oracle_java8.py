44ac867c5b59422d14292c4bc0c507dafcea6f1c
Francis Kayiwa <kayiwa@princeton.edu>
2019-01-23 12:59:19 -0500

    write tests

--- roles/pulibrary.oracle.java8/molecule/default/tests/test_oracle_java8.py
+++ roles/pulibrary.oracle.java8/molecule/default/tests/test_oracle_java8.py
@@ -1,9 +1,21 @@
 import os
+import pytest
 
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+@pytest.mark.parametrize("name", [
+    "oracle-java8-installer"
+    "ca-certificates"
+    "oracle-java8-set-default"
+    ])
+def test_for_oracle_java8_software(host, name):
+    pkg = host.package(name)
+
+    assert pkg.is_installed
 
 
 def test_hosts_file(host):
