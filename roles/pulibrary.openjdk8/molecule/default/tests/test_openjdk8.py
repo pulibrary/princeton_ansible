dfda02ac10d97409381eaef778ae5fb528c3ad7c
Francis Kayiwa <kayiwa@princeton.edu>
2019-01-23 13:17:25 -0500

    add tests

--- roles/pulibrary.openjdk8/molecule/default/tests/test_openjdk8.py
+++ roles/pulibrary.openjdk8/molecule/default/tests/test_openjdk8.py
@@ -1,9 +1,19 @@
 import os
+import pytest
 
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+@pytest.mark.parametrize("name", [
+    "openjdk-8-jdk"
+    ])
+def test_for_openjdk8(host, name):
+    pkg = host.package(name)
+
+    assert pkg.is_installed
 
 
 def test_hosts_file(host):
