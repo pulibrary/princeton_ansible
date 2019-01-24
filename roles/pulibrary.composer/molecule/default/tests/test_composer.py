504cea7ad4d26a2761fdfbc2a616a1c602d6bdc2
Francis Kayiwa <kayiwa@princeton.edu>
2019-01-24 09:40:11 -0500

    add drush tests

--- roles/pulibrary.composer/molecule/default/tests/test_composer.py
+++ roles/pulibrary.composer/molecule/default/tests/test_composer.py
@@ -6,7 +6,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def confirm_composer_file(host):
+def test_composer_file(host):
     file = host.file("/usr/local/bin/composer")
 
     assert file.exists
