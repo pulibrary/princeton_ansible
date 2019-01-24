5ec81458d3bd6e6f6d3c76b6fe393832a7d215d0
Francis Kayiwa <kayiwa@princeton.edu>
2019-01-24 09:40:11 -0500

    add drush tests

--- roles/pulibrary.drush/molecule/default/tests/test_drush.py
+++ roles/pulibrary.drush/molecule/default/tests/test_drush.py
@@ -6,6 +6,12 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
+def test_drush_file(host):
+    file = host.file("/usr/local/bin/drush")
+
+    assert file.exists
+
+
 def test_hosts_file(host):
     f = host.file('/etc/hosts')
 
