15239feccb6527addf04ba291c51864d94617a7f
Francis Kayiwa <kayiwa@princeton.edu>
2019-01-24 11:37:20 -0500

    create a db configuration repo

---
+++ roles/pulibrary.mariadbserver/molecule/default/tests/test_mariadb.py
@@ -0,0 +1,31 @@
+import os
+import pytest
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+@pytest.mark.parametrize("name", [
+    "python-mysqldb",
+    "mariadb-server",
+    ])
+def test_for_mariadb_software(host, name):
+    pkg = host.package(name)
+
+    assert pkg.is_installed
+
+
+def test_mariadb_listening_port(host):
+    socket = host.socket("tcp://127.0.0.1:3306")
+
+    assert socket.is_listening
+
+
+def test_hosts_file(host):
+    f = host.file('/etc/hosts')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
