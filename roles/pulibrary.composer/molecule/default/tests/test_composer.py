1bd33c3116328fb2e6de31d46a94b348569c6fb3
Francis Kayiwa <kayiwa@princeton.edu>
2019-01-24 09:24:33 -0500

    add idempotency to this task

--- roles/pulibrary.composer/molecule/default/tests/test_composer.py
+++ roles/pulibrary.composer/molecule/default/tests/test_composer.py
@@ -6,6 +6,12 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
+def confirm_composer_file(host):
+    file = host.file("/usr/local/bin/composer")
+
+    assert file.exists
+
+
 def test_hosts_file(host):
     f = host.file('/etc/hosts')
 
