92901151547566064836c6ec2dfaa78ce7746890
Francis Kayiwa <kayiwa@princeton.edu>
2019-01-24 20:33:27 -0500

    refactor role that only installs the client

---
+++ roles/pulibrary.mariadb/molecule/default/tests/test_mariadb.py
@@ -0,0 +1,25 @@
+import os
+import pytest
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+@pytest.mark.parametrize("name", [
+    "python-mysqldb",
+    "mysql-client",
+    ])
+def test_for_mysql_client_software(host, name):
+    pkg = host.package(name)
+
+    assert pkg.is_installed
+
+
+def test_hosts_file(host):
+    f = host.file('/etc/hosts')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
