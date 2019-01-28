d209c1bdcf261adf0e244c98dcd806a8043cd2fc
Francis Kayiwa <kayiwa@princeton.edu>
2019-01-28 12:01:20 -0500

    fix idempotency in the role

---
+++ roles/pulibrary.redis/molecule/default/tests/test_redis.py
@@ -0,0 +1,18 @@
+import os
+import pytest
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+@pytest.mark.parametrize("name", [
+    "redis-server",
+    "redis-tools",
+    "python-redis",
+    ])
+def test_commonly_needed_build_files(host, name):
+    pkg = host.package(name)
+
+    assert pkg.is_installed
