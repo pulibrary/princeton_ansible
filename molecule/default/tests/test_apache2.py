4807c15190d9444c86722e56dc20d535691c755a
Francis Kayiwa <kayiwa@princeton.edu>
2019-02-01 15:13:49 -0500

    merge from master

--- molecule/default/tests/test_apache2.py
+++ molecule/default/tests/test_apache2.py
@@ -1,1 +1,18 @@
-../roles/pulibrary.apache2/molecule/default/tests/test_apache2.py
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_is_apache_installed(host):
+    pkg = host.package('apache2')
+
+    assert pkg.is_installed
+
+
+def test_apache_listening_http(host):
+    socket = host.socket('tcp://0.0.0.0:80')
+
+    assert socket.is_listening
