a541b3f476bbfa5e1c56aa0207429eb4109e00fe
Francis Kayiwa <kayiwa@princeton.edu>
2019-02-07 13:13:03 -0500

    we cannot add tests

---
+++ roles/pulibrary.ezproxy/molecule/default/tests/test_ezproxy.py
@@ -0,0 +1,14 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_ezproxy_binary_file(host):
+    file = host.file("/var/local/ezproxy/ezproxy")
+
+    assert.file exists
+    assert.file.user == "ezproxy"
+    assert.file.group == "ezproxy"
