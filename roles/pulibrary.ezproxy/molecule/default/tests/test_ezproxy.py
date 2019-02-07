842d9ce3204d61f807cd45074908abf422269731
Francis Kayiwa <kayiwa@princeton.edu>
2019-02-07 14:00:37 -0500

    unbreak travis

--- roles/pulibrary.ezproxy/molecule/default/tests/test_ezproxy.py
+++ roles/pulibrary.ezproxy/molecule/default/tests/test_ezproxy.py
@@ -9,6 +9,6 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 def test_ezproxy_binary_file(host):
     file = host.file("/var/local/ezproxy/ezproxy")
 
-    assert.file exists
-    assert.file.user == "ezproxy"
-    assert.file.group == "ezproxy"
+    assert file.exists
+    assert file.user == "ezproxy"
+    assert file.group == "ezproxy"
