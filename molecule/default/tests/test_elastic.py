49c3cf0c173f37108f58ceff1a9ad271c99da490
Francis Kayiwa <kayiwa@princeton.edu>
2019-03-20 09:19:01 -0400

    add openjdk tests

--- molecule/default/tests/test_elastic.py
+++ molecule/default/tests/test_elastic.py
@@ -1,12 +1,1 @@
-import os
-
-import testinfra.utils.ansible_runner
-
-testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
-
-
-def test_if_elasticsearch_installed(host):
-    pkg = host.package('elasticsearch')
-
-    assert pkg.is_installed
+../roles/pulibrary.elastic/molecule/default/tests/test_elastic.py
