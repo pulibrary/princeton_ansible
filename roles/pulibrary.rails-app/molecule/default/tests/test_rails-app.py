0168abcd6dfe0ef68456ea10f6b4e3e565f46c90
Carolyn Cole <cam156@psu.edu>
2019-04-29 15:44:00 -0400

    Adding tests to the rails app
    Additionally added a listen port to passenger so it would not fail converge.  Docker is alreday using port 80, so it failed when trying to start nginx if it also runs on the default port 80.
    
    Co-authored-by: Anna Headley <anna.headley@gmail.com>
    Co-authored-by: Francis Kayiwa <fkayiwa@princeton.edu>

---
+++ roles/pulibrary.rails-app/molecule/default/tests/test_rails-app.py
@@ -0,0 +1,38 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_for_rails_app_rails_config(host):
+    f = host.file('/home/deploy/app_configs/rails_app')
+
+    assert f.exists
+    assert f.user == 'deploy'
+    assert f.group == 'deploy'
+
+
+def test_for_rails_app_is_directory(host):
+    f = host.file('/opt/rails_app/shared/tmp')
+
+    assert f.exists
+    assert f.is_directory
+    assert f.user == 'deploy'
+    assert f.group == 'deploy'
+
+
+def test_for_rails_app_bind9_is_available(host):
+    p = host.package('bind9')
+
+    assert p.is_installed
+    assert host.process.get(user="bind", comm="named")
+
+
+def test_for_rails_app_logrotate(host):
+    f = host.file('/etc/logrotate.d/rails_app')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
