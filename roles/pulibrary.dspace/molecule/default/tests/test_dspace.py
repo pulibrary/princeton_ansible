65967f6ffe523cbd496e542894d9f7015beb0357
Francis Kayiwa <kayiwa@princeton.edu>
2019-05-24 11:12:16 -0400

    add tests for dspace

--- roles/pulibrary.dspace/molecule/default/tests/test_dspace.py
+++ roles/pulibrary.dspace/molecule/default/tests/test_dspace.py
@@ -6,9 +6,10 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_for_dspace_download(host):
+    f = host.file('/opt/dspace-6.3-release/dspace')
 
     assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert f.is_directory
+    assert f.user == 'deploy'
+    assert f.group == 'deploy'
