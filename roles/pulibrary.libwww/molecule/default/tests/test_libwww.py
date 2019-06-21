13fd44b7ac0d5079c7f5b37109bee08cda5b7ac9
Carolyn Cole <cam156@psu.edu>
2019-06-21 14:11:07 -0400

    getting the site fully working
    Adds updates to database for configuring SMTP, CAS cert, and search api
    Updates ability to write to drupal mount

--- roles/pulibrary.libwww/molecule/default/tests/test_libwww.py
+++ roles/pulibrary.libwww/molecule/default/tests/test_libwww.py
@@ -1,4 +1,5 @@
 import os
+import pytest
 
 import testinfra.utils.ansible_runner
 
@@ -6,7 +7,24 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_discovery_utils_home_directory(host):
-    file = host.file("/var/www/discoveryutils/index.php")
+@pytest.mark.parametrize("file", [
+    "/var/www/discoveryutils/public/index.php",
+    "/var/www/library/sites/default/settings.php",
+    "/etc/drush/aliases.drushrc.php",
+    "/var/www/discoveryutils/vendor/symfony",
+    "/var/www/discoveryutils/.env.local"
+    ])
+def test_for_libwww_file_exist(host, file):
+    file = host.file(file)
 
     assert file.exists
+
+
+@pytest.mark.parametrize("line", [
+    "Alias /utils /var/www/discoveryutils/public",
+    "DocumentRoot /var/www/library"
+    ])
+def test_for_libwww_apache_config(host, line):
+    file = host.file("/etc/apache2/sites-available/000-default.conf")
+
+    assert file.contains(line)
