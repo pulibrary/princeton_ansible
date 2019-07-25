0053881e4593a8bc4d15515316d409c89f11de14
Carolyn Cole <cam156@psu.edu>
2019-07-25 13:58:22 -0400

    Adding molecule to the extra_path role

---
+++ roles/pulibrary.extra_path/molecule/default/tests/test_default.py
@@ -0,0 +1,13 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_hosts_file(host):
+    file = host.file('/etc/environment')
+
+    assert file.exists
+    assert file.contains('/test/bin')
