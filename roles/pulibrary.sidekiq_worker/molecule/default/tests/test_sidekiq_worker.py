c1958fc47c64f53337ae89dd50ebbe85363090b7
Francis Kayiwa <kayiwa@princeton.edu>
2019-07-28 14:16:23 -0400

    fix idempotency

--- roles/pulibrary.sidekiq_worker/molecule/default/tests/test_sidekiq_worker.py
+++ roles/pulibrary.sidekiq_worker/molecule/default/tests/test_sidekiq_worker.py
@@ -6,7 +6,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
+def test_sidekiq_workers_systemd_service_file(host):
     f = host.file('/etc/systemd/system/sidekiq-workers.service')
 
     assert f.exists
