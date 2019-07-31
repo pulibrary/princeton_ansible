994ebcdd4154d1eec5b4f75ee9071a27bd9cafe9
Francis Kayiwa <kayiwa@princeton.edu>
2019-07-30 20:20:45 -0400

    add testss

--- roles/pulibrary.sneakers_worker/molecule/default/tests/test_sneakers_workers.py
+++ roles/pulibrary.sneakers_worker/molecule/default/tests/test_sneakers_workers.py
@@ -6,8 +6,8 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_sneakers_workers_systemd_service_file(host):
+    f = host.file('/etc/systemd/system/sneakers.service')
 
     assert f.exists
     assert f.user == 'root'
