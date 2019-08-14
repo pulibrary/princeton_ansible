8602c3763256472c14a79bf4c29ff5ae48e28c29
Carolyn Cole <cam156@psu.edu>
2019-08-14 10:27:58 -0400

    Adding a role for recap ran molecule init role -r roles/pulibrary.recap-www updated to run using the example files Moved shared items to drupal role created a capistrano.yml in the drupal role for items needed for capistrano updated the recap role to have the correct settings.php

--- roles/pulibrary.drupal/molecule/default/tests/test_drupal.py
+++ roles/pulibrary.drupal/molecule/default/tests/test_drupal.py
@@ -1,4 +1,5 @@
 import os
+import pytest
 
 import testinfra.utils.ansible_runner
 
@@ -6,7 +7,36 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_drupal_home_directory(host):
-    file = host.file("/var/www/drupal/index.php")
+@pytest.mark.parametrize("line", [
+    "deploy ALL=(ALL) NOPASSWD: /usr/sbin/service apache2 *",
+    "Runas_Alias WWW = www-data",
+    "deploy ALL = (WWW) NOPASSWD: ALL",
+    "deploy ALL=(ALL) NOPASSWD: /bin/chown -R www-data /var/www/drupal*",
+    "deploy ALL=(ALL) NOPASSWD: /bin/chown -R deploy /var/www/drupal*"
+    ])
+def test_for_libwww_sudoer(host, line):
+    file = host.file("/etc/sudoers")
+
+    assert file.contains(line)
+
+
+@pytest.mark.parametrize("file", [
+    "/home/deploy/settings.php",
+    "/etc/drush/aliases.drushrc.php"
+    ])
+def test_for_libwww_file_exist(host, file):
+    file = host.file(file)
 
     assert file.exists
+
+
+@pytest.mark.parametrize("name", [
+    "php7.2",
+    "php7.2-common",
+    "php7.2-mbstring",
+    "sendmail"
+    ])
+def test_for_pas_php_software(host, name):
+    pkg = host.package(name)
+
+    assert pkg.is_installed
