37d9de3e70337ac07c387ba336956ca803b675a7
Francis Kayiwa <kayiwa@princeton.edu>
2019-08-26 11:58:23 -0400

    unbreak failing tests

--- roles/pulibrary.rvm/molecule/default/tests/test_rvm.py
+++ roles/pulibrary.rvm/molecule/default/tests/test_rvm.py
@@ -6,24 +6,8 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-<<<<<<< HEAD
 def test_rvm_file(host):
     f = host.file('/usr/local/rvm')
-=======
-<<<<<<< HEAD:roles/pulibrary.drush/molecule/default/tests/test_drush.py
-def test_drush_file(host):
-    file = host.file("/usr/local/bin/drush")
-
-    assert file.exists
-
-
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
-=======
-def test_rvm_file(host):
-    f = host.file('/usr/local/rvm')
->>>>>>> add rvm tests:roles/pulibrary.rvm/molecule/default/tests/test_rvm.py
->>>>>>> add rvm tests
 
     assert f.exists
     assert f.user == 'root'
