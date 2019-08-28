1845a8ecec37e3d448715b78fba4bd6a103927ba
Francis Kayiwa <kayiwa@princeton.edu>
2019-08-28 10:29:49 -0400

    add tests back in

--- roles/pulibrary.mariadb/molecule/default/tests/test_mariadb.py
+++ roles/pulibrary.mariadb/molecule/default/tests/test_mariadb.py
@@ -15,3 +15,9 @@ def test_for_mysql_client_software(host, name):
     pkg = host.package(name)
 
     assert pkg.is_installed
+
+
+def test_for_mysql_user_and_access(host):
+    command = "mysql -usome_user -pchange_me some_database -e'show tables'"
+    cmd = host.run(command)
+    assert "ERROR " not in cmd.stderr
