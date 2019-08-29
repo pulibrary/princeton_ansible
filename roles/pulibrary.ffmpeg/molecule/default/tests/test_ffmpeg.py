63672523197be3d4dfb0b13d3d13ca465daeb686
Francis Kayiwa <kayiwa@princeton.edu>
2019-08-29 09:56:30 -0400

    add tests for ffmpeg and circle config

--- roles/pulibrary.ffmpeg/molecule/default/tests/test_ffmpeg.py
+++ roles/pulibrary.ffmpeg/molecule/default/tests/test_ffmpeg.py
@@ -6,9 +6,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_is_ffmpeg_installed(host):
+    pkg = host.package('ffmpeg')
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert pkg.is_installed
