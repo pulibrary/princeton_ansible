65d4bd7e2af23f3c32f20bd181c483d43c5f5702
Carolyn Cole <cam156@psu.edu>
2019-09-03 12:22:06 -0400

    Changing staff cron job Setting time to run once at 5 am, instead of every minute in the 5am hour Adding the path to drush to allow for cron to run it Adding an output file

--- roles/pulibrary.libwww/files/get_staff_updates.sh
+++ roles/pulibrary.libwww/files/get_staff_updates.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-DRP_FILES_PATH=`drush dd @prod:%files/feeds/`
+DRP_FILES_PATH=`/usr/local/bin/drush dd @prod:%files/feeds/`
 curl http://libweb5.princeton.edu/NewStaff/LibraryDirectoryPrimary.csv  > $DRP_FILES_PATH/LibraryDirectoryPrimary.csv
