6b413d1787f5d560e37999f473cea9fc458b8f54
Francis Kayiwa <kayiwa@princeton.edu>
2019-09-09 16:16:30 -0400

    add tests and add to circleci

--- roles/pulibrary.blacklight-app/molecule/default/tests/test_default.py
+++ roles/pulibrary.blacklight-app/molecule/default/tests/test_default.py
@@ -6,9 +6,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_rails_app_dir(host):
+    f = host.file('/opt/rails_app')
 
     assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
