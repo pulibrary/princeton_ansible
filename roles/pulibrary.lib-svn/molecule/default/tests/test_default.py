0f8ea85fde5a50510a6bf0d12d5cdaf737e28bc6
Francis Kayiwa <kayiwa@pobox.com>
2019-09-18 11:43:09 -0400

    create subversion repo

--- roles/pulibrary.lib-svn/molecule/default/tests/test_default.py
+++ roles/pulibrary.lib-svn/molecule/default/tests/test_default.py
@@ -6,9 +6,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_lib_svn_home_dir(host):
+    f = host.file('/var/svn/libsvn')
 
     assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
