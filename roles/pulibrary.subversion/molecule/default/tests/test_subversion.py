8719cdc30d15be94ca55be7db1b42f16e32b9517
Francis Kayiwa <kayiwa@princeton.edu>
2019-09-19 13:47:52 -0500

    add subversion tests

--- roles/pulibrary.subversion/molecule/default/tests/test_subversion.py
+++ roles/pulibrary.subversion/molecule/default/tests/test_subversion.py
@@ -6,9 +6,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_if_subversion_is_installed(host):
+    pkg = host.package('subversion')
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert pkg.is_installed
