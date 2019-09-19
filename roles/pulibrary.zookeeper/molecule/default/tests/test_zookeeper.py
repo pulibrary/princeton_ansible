b6afa775cd88150b5e75aa5ad14754628aef20c0
Eliot Jordan <eliotj@princeton.edu>
2019-09-19 14:21:58 -0500

    754 zookeeper molecule  (#855)
    * initial molecularization
    
    * manage systemd container problems
    
    * add tests
    
    * openjdk dependencies
    
    * Add zookeeper to actions

---
+++ roles/pulibrary.zookeeper/molecule/default/tests/test_zookeeper.py
@@ -0,0 +1,17 @@
+import os
+import pytest
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+@pytest.mark.parametrize("name", [
+    "zookeeper",
+    "zookeeperd",
+    ])
+def test_for_zookeeper_packages(host, name):
+    pkg = host.package(name)
+
+    assert pkg.is_installed
