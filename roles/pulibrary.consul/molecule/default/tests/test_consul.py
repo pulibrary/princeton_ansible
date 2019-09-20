f56d6c82ac47247fe3461b4aeb8f65bcd8202fa6
Francis Kayiwa <kayiwa@pobox.com>
2019-09-20 14:40:27 -0400

    add consul role

--- roles/pulibrary.consul/molecule/default/tests/test_consul.py
+++ roles/pulibrary.consul/molecule/default/tests/test_consul.py
@@ -6,8 +6,8 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_consul_bin_file(host):
+    f = host.file('/usr/local/bin/consul')
 
     assert f.exists
     assert f.user == 'root'
