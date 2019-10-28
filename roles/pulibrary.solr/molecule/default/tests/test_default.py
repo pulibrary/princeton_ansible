8bad0bc3b4667a6bc7ff45679bb34e8b55ef95f5
Trey Pendragon <tpendragon@princeton.edu>
2019-10-28 14:56:51 -0700

    Add some basic tests for Solr role.

--- roles/pulibrary.solr/molecule/default/tests/test_default.py
+++ roles/pulibrary.solr/molecule/default/tests/test_default.py
@@ -13,3 +13,10 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+
+def test_solr_running(host):
+    command = """curl --digest -L -D - http://localhost:8983/solr"""
+    cmd = host.run(command)
+
+    assert 'HTTP/1.1 200 OK' in cmd.stdout
