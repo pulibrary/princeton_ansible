5bf035b83356322918913a89366aaac2b8c322c6
Francis Kayiwa <kayiwa@pobox.com>
2019-12-20 09:01:46 -0500

    add saxon tests

--- roles/pulibrary.saxon/molecule/default/tests/test_default.py
+++ roles/pulibrary.saxon/molecule/default/tests/test_default.py
@@ -7,8 +7,16 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_saxon9_xquery_file(host):
+    f = host.file('/usr/local/bin/saxon9he-xquery')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+
+
+def test_saxon9_xslt_file(host):
+    f = host.file('/usr/local/bin/saxon9he-xslt')
 
     assert f.exists
     assert f.user == 'root'
