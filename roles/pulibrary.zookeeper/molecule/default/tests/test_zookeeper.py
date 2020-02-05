09b0ff58ab27b461b8f85e27de77c403c27417f0
Anna Headley <anna.headley@gmail.com>
2020-02-05 09:08:19 -0500

    Update solr / zookeeper variables
    They were a bit of a jumble. Now the zk hosts dict list is always a
    list, and the znode is added to the zk hosts string if it exists.
    Hopefully the new names are easier to read / parse.

--- roles/pulibrary.zookeeper/molecule/default/tests/test_zookeeper.py
+++ roles/pulibrary.zookeeper/molecule/default/tests/test_zookeeper.py
@@ -6,7 +6,6 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-
 @pytest.mark.parametrize("name", [
     "zookeeper",
     "zookeeperd",
@@ -15,3 +14,8 @@ def test_for_zookeeper_packages(host, name):
     pkg = host.package(name)
 
     assert pkg.is_installed
+
+def test_myid_configuration(host):
+    file = host.file("/etc/zookeeper/conf/myid")
+
+    assert file.contains('1')
