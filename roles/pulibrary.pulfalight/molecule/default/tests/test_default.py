2a1fd18e4a722d8e71caf28921d2c11876eae2ec
Anna Headley <anna.headley@gmail.com>
2020-03-30 10:24:04 -0400

    Remove local solr from pulfalight role
    It's now using the solr 8 cloud

--- roles/pulibrary.pulfalight/molecule/default/tests/test_default.py
+++ roles/pulibrary.pulfalight/molecule/default/tests/test_default.py
@@ -13,17 +13,3 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
-
-
-def test_solr_core_running(host):
-    command = """curl http://localhost:8983/solr/admin/cores?action=STATUS"""
-    cmd = host.run(command)
-
-    assert 'pulfalight-staging' in cmd.stdout
-
-
-def test_solr_config_installed(host):
-    command = """curl http://localhost:8983/solr/pulfalight-staging/config"""
-    cmd = host.run(command)
-
-    assert 'collection_title_tesim^150' in cmd.stdout
