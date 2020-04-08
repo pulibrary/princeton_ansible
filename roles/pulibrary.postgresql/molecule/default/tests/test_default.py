5c8bec9ed75eadbabab586703a565ad1f8bf5693
Francis Kayiwa <kayiwa@princeton.edu>
2020-04-08 19:53:37 -0400

    add example playbook to install databases use variables to add a database from the roles

--- roles/pulibrary.postgresql/molecule/default/tests/test_default.py
+++ roles/pulibrary.postgresql/molecule/default/tests/test_default.py
@@ -1,15 +1,23 @@
 import os
+import pytest
 
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']
-).get_hosts('all')
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+@pytest.mark.parametrize("name", [
+    "postgresql-12",
+    "postgresql-client-12",
+    ])
+def test_for_postgresql_server_software(host, name):
+    pkg = host.package(name)
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert pkg.is_installed
+
+
+def test_mariadb_server_listening_port(host):
+    socket = host.socket("tcp://127.0.0.1:5432")
+
+    assert socket.is_listening
