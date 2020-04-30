845f917f9274d012a0423b71f695b0770ed880bc
Carolyn Cole <cac9@princeton.edu>
2020-04-30 09:26:41 -0400

    Upping the varriables to stop errors

--- roles/pulibrary.mariadb/files/permissive.sql
+++ roles/pulibrary.mariadb/files/permissive.sql
@@ -1,3 +1,5 @@
 SET GLOBAL pxc_strict_mode=PERMISSIVE;
 SET GLOBAL log_bin_trust_function_creators = 1;
-SET GLOBAL max_allowed_packet=128*1024*1024;
+SET GLOBAL max_allowed_packet=256*1024*1024;
+SET GLOBAL wait_timeout=2147483;
+SET GLOBAL max_connections=2*4096;
