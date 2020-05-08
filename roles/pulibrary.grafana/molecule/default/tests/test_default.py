60a7d3a872aca0a8de4c388eb1e617f56310f30d
Francis Kayiwa <kayiwa@pobox.com>
2020-05-08 16:54:30 -0400

    add tests for the role

--- roles/pulibrary.grafana/molecule/default/tests/test_default.py
+++ roles/pulibrary.grafana/molecule/default/tests/test_default.py
@@ -3,13 +3,16 @@ import os
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']
-).get_hosts('all')
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_if_grafana_installed(host):
+    pkg = host.package('grafana')
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert pkg.is_installed
+
+
+def test_if_grafana_listening_http(host):
+    socket = host.socket('tcp://127.0.0.1:3000')
+
+    assert socket.is_listening
