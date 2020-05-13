3129bda48d2eccd4fa310b877ec1b46b13768f68
Francis Kayiwa <kayiwa@pobox.com>
2020-05-13 14:04:32 -0400

    add apache configuration

--- roles/pulibrary.ojs/molecule/default/tests/test_default.py
+++ roles/pulibrary.ojs/molecule/default/tests/test_default.py
@@ -6,9 +6,15 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_ojs_symlink_file(host):
+    f = host.file('/var/www/ojs')
 
     assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+
+
+def test_for_ojs_db_access(host):
+    command = "sudo -u postgres psql -l"
+    cmd = host.run(command)
+
+    assert "ERROR " not in cmd.stderr
+    assert "ojs_db" in cmd.stdout
