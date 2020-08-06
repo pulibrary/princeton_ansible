bcd0c9406b0206989719f6c029734dc9fd39f084
Francis Kayiwa <kayiwa@pobox.com>
2020-08-06 17:30:30 -0400

    add the dataspace role
    clean up the shibboleth contact user
    add dspace tests
    add dataspace staging VM

--- roles/pulibrary.dspace/molecule/default/tests/test_dspace.py
+++ roles/pulibrary.dspace/molecule/default/tests/test_dspace.py
@@ -6,9 +6,9 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_for_dspace_download(host):
-    f = host.file('/etc/hosts')
+def test_for_dspace_atmire_aliases(host):
+    f = host.file('/home/dspace/.aliases.local')
 
     assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert f.user == 'dspace'
+    assert f.group == 'dspace'
