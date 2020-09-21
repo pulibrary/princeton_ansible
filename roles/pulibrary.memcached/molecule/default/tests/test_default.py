bf5eb2629334a374e2d7a8a1beaf3b78b06e7325
Trey Pendragon <tpendragon@princeton.edu>
2020-09-21 15:58:30 -0700

    Add test.

--- roles/pulibrary.memcached/molecule/default/tests/test_default.py
+++ roles/pulibrary.memcached/molecule/default/tests/test_default.py
@@ -13,3 +13,10 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+
+def test_memcached_installed(host):
+    command = """memcached -V"""
+    cmd = host.run(command)
+
+    assert 'memcached 1.5.6' in cmd.stdout
