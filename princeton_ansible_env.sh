8bd4d64945225c36b7aed76bd451561de1ad6888
Francis Kayiwa <kayiwa@pobox.com>
2025-08-14 09:12:03 -0400

    remove pipfile
    remove old configuration files for mise/asdf
    remove configurations for pipenv
    change shell scripts for first time use to rely on devbox

--- princeton_ansible_env.sh
+++ princeton_ansible_env.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
-# export our environment
+# Princeton Ansible Environment Setup for Devbox
+# This script is automatically executed when entering the Devbox shell
+# but can also be sourced manually if needed
+
+# Export our environment
 ANSIBLE_VAULT_PASSWORD_FILE="$(command -v lastpass-ansible)"
 export ANSIBLE_VAULT_PASSWORD_FILE
-# retain our env for 9 hours
+
+# Retain our env for 9 hours
 LPASS_AGENT_TIMEOUT="32400"
 export LPASS_AGENT_TIMEOUT
-# protect vault commits
+
+# Protect vault commits
 ./bin/setup
+
+# Optional: Display environment info
+echo "Ansible environment configured:"
+echo "  ANSIBLE_VAULT_PASSWORD_FILE: $ANSIBLE_VAULT_PASSWORD_FILE"
+echo "  LPASS_AGENT_TIMEOUT: $LPASS_AGENT_TIMEOUT (9 hours)"
--- princeton_ansible_fish_env.sh
+++ princeton_ansible_fish_env.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env fish
-# export our environment
+# Princeton Ansible Environment Setup for Devbox (Fish shell)
+# This script can be sourced when using fish shell
+
+# Export our environment
 set -x ANSIBLE_VAULT_PASSWORD_FILE (command -v lastpass-ansible)
-# retain our env for 9 hours
+
+# Retain our env for 9 hours
 set -x LPASS_AGENT_TIMEOUT 32400
-# protect vault commits
+
+# Protect vault commits
 ./bin/setup
+
+# Optional: Display environment info
+echo "Ansible environment configured:"
+echo "  ANSIBLE_VAULT_PASSWORD_FILE: $ANSIBLE_VAULT_PASSWORD_FILE"
+echo "  LPASS_AGENT_TIMEOUT: $LPASS_AGENT_TIMEOUT (9 hours)"
