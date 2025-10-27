3338664e7a689a52f875d6417572ad54856fb337
Francis Kayiwa <kayiwa@users.noreply.github.com>
2025-10-27 12:25:19 -0400

    migrate to devbox (#6436)
    * remove pipfile
    
    remove old configuration files for mise/asdf
    remove configurations for pipenv
    change shell scripts for first time use to rely on devbox
    * add the devbox json file
    * add a cross shell devbox initialization script
    * add a fish shell helper script
    * replace README to use devbox
    * Use devbox's recommended VENV_DIR
    * make devbox_init use absolute paths
    
    make sure the initialize script does not pick up the systems pip
    * fix ruby path
    * add the lock file to ensure it is reproducible
    * on a new macOS git is not configured
    
    location of a brand new macOS broke this. let's unify our version of git
    * remove devbox_init and fish activator; add cross-shell init
    * create a posix compliant devbox cross-shell initialization script so devbox shell works in bash/zsh/fish without custom per-shell glue
    * remove activate_venv.fish
    * point `devbox.json` init_hook to `./bin/devbox-init`
    * Make `update-depts` use the venv's Python explicitly
    * remove shell-specific activation guidance
    
    remove references to `.devbox_init.sh`. the venv is autocreated and used by devbox
    keeps ANSIBLE_VAULT_PASSWORD file set via devbox to the ruby virtenv path and ensure that the gem is installed on shell entry
    
    * we typically (ever?) don't rule vault with molecule
    * we never run vault on molecule
    
    vault and molecule should not touch the lastpass session
    * these will be set by devbox
    * molecule does not need lastpass
    
    initialize devbox for all shells.
    run devbox init as a separate step from init-hook
    * update lock, cleanup README
    
    ---------
    
    Co-authored-by: Alicia Cozine<acozine@users.noreply.github.com>
    Co-authored-by: Carolyn Cole <carolyncole@users.noreply.github.com>
    Co-authored-by: Precilla Prempeh <precillap@users.noreply.github.com>
    Co-authored-by: Ryan Laddusaw <rladdusaw@users.noreply.github.com>
    Co-authored-by: Trey Pendragon <tpendragon@princeton.edu>
    Co-authored-by: Vickie Karasic <vickiekarasic@users.noreply.github.com>

--- princeton_ansible_fish_env.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env fish
-# export our environment
-set -x ANSIBLE_VAULT_PASSWORD_FILE (command -v lastpass-ansible)
-# retain our env for 9 hours
-set -x LPASS_AGENT_TIMEOUT 32400
-# protect vault commits
-./bin/setup
